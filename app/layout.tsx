e2df8a6307c30e776f80f9f2e78a3d98ec93ffc0
UPSOL <upsoltechnologies@gmail.com>
2025-01-30 00:23:21 +0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import localFont from 'next/font/local'
-import {FaWindowClose} from "react-icons/fa";
+
 const biennale = localFont({
     src: [
         {
