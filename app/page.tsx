09fdd25534b837ea168a9b83e5a36ed5a23dbd11
UPSOL <upsoltechnologies@gmail.com>
2025-01-30 00:25:52 +0500

    fixes 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
-import Image from "next/image";
-import Placeholder from "@/components/layout/Placeholder";
+import PageTitleImage from "@/components/layout/PageTitleImage";
 
 export default function Home() {
 
     return (
         <div className="flex flex-col items-center justify-center w-full">
-            <Placeholder title={(<>202<span className="font-sans">4</span></>)}/>
-            <h2 className="font-extrabold text-7xl">XRPH <span className="font-sans">@</span> 202<span
-                className="font-sans">4</span></h2>
-            <Image src={'/waves.webp'} alt='XRPh' width={400} height={40}/>
-
+            <PageTitleImage/>
         </div>
     );
 }
