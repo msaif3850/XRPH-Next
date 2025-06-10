5998b7e45dc39d547d69ad5b897ff83c33dc28c3
UPSOL <upsoltechnologies@gmail.com>
2025-06-10 14:02:22 +0500

    Roadmap page text

--- components/layout/RoadmapTimeline.tsx
+++ components/layout/RoadmapTimeline.tsx
@@ -13,14 +13,14 @@ const milestones = [
                     {text: 'XRP Healthcare Global Announcement', checked: true},
                     {text: 'XRPH AI App launched on Google Play and Apple Store', checked: true},
                     {text: 'Encryption layer implemented to ensure user data protection', checked: true},
-                    {text: '1.2M XRPH distributed as staking rewards', checked: true},
+                    {text: ' 1.2M XRPH distributed as staking rewards', checked: true},
                 ].map((item, i) => (
                     <li key={i} className="flex items-start">
                         <div>
                             {item.checked ? (
                                 <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -42,18 +42,22 @@ const milestones = [
                     {text: 'Existing teams retained and overseen by corporate management', checked: true},
                     {text: 'Upgrades initiated for acquired businesses services, and equipment', checked: true},
                     {text: 'Image feature launched on the XRPH AI App for mobile and web platforms', checked: true},
-                    {text: 'XRPH & RLUSD token ready to use as a digital payment method for products and services within the ecosystem', checked: true},
+                    {
+                        text: 'XRPH & RLUSD token ready to use as a digital payment method for products and services within the ecosystem',
+                        checked: true
+                    },
                     {text: 'Streamlined M&A operations to achieve cost savings and efficiency', checked: true},
                     {text: 'Ongoing upgrades to healthcare facilities', checked: true},
                     {text: '700K XRPH distributed as staking rewards in March', checked: true},
-                    {text: '870K XRPH distributed as staking rewards in April', checked: false},
+                    {text: '870K XRPH distributed as staking rewards in April', checked: true},
+                    {text: '804K XRPH distributed as staking rewards in May', checked: true},
                 ].map((item, i) => (
                     <li key={i} className="flex items-start">
                         <div>
                             {item.checked ? (
                                 <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -72,27 +76,28 @@ const milestones = [
                 {[
                     {
                         text: "Doctor search and connect feature launched in the XRPH AI App for mobile and web platforms",
-                        checked: false
+                        checked: true
                     },
+                    {text: "Integrated XRP Healthcare Prescription Savings Card into XRPH AI App.", checked: true},
+                    {text: "Pharmacy search and connect feature added to XRPH AI App.", checked: true},
                     {
                         text: "Expanded due diligence conducted for identifying new healthcare investment targets",
-                        checked: false
+                        checked: true
                     },
                     {text: "Partnerships established with international healthcare organizations", checked: true},
                     {
                         text: "Further development and feature enhancements for the XRPH AI App and XRPH Wallet",
                         checked: false
                     },
-                    {text: "804K XRPH distributed as staking rewards in May", checked: false},
                     {text: "1.1M XRPH distributed as staking rewards in June", checked: false},
-
+                    {text: "XRP Healthcare to be listed on a Canadian stock exchange (IPO)", checked: false},
                 ].map((item, i) => (
                     <li key={i} className="flex items-start">
                         <div>
                             {item.checked ? (
-                                <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
+                                <FaCheckDouble className="text-sm text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -114,10 +119,7 @@ const milestones = [
                     {text: "Strategic promotion and marketing of M&A activities across Africa", checked: false},
                     {text: "Continued upgrades to healthcare facilities, services, and equipment", checked: false},
                     {text: "XRPH token usage expanded across the growing healthcare network", checked: false},
-                    {
-                        text: "Strengthened international partnerships to solidify the XRP Healthcare ecosystem",
-                        checked: false
-                    },
+                    {text: "Strengthened international partnerships to solidify the XRP Healthcare ecosystem", checked: false},
                     {text: "285K XRPH distributed as staking rewards in July", checked: false},
 
                 ].map((item, i) => (
@@ -126,7 +128,7 @@ const milestones = [
                             {item.checked ? (
                                 <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -143,26 +145,19 @@ const milestones = [
         description: (
             <ul className="space-y-3">
                 {[
-                    {
-                        text: "Increase the value of our portfolio by implementation of growth strategies",
-                        checked: false
-                    },
-                    {
-                        text: "Expand into new markets and territories by diversifying the XRP Healthcare portfolio",
-                        checked: false
-                    },
+                    {text: "Increase the value of our portfolio by implementation of growth strategies", checked: false},
+                    {text: "Expand into new markets and territories by diversifying the XRP Healthcare portfolio",checked: false},
                     {text: "Grow revenue and profitability of the company", checked: false},
-                    {
-                        text: "XRPH token accepted for payment and services rendered at all XRP Healthcare pharmacies, medical centres and hospitals",
-                        checked: false
-                    },
+                    {text: "Integrate targets post merger and expand output through CAPEX and streamlined operations", checked: false},
+                    {text: "Second Strategic Acquisition Secured", checked: false},
+                    {text: "XRPH token accepted for payment and services rendered at all XRP Healthcare pharmacies, medical centres and hospitals", checked: false},
                 ].map((item, i) => (
                     <li key={i} className="flex items-start">
                         <div>
                             {item.checked ? (
                                 <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -181,14 +176,13 @@ const milestones = [
                 {[
                     {text: "Preparation begins to prepare XRP Healthcare for IPO", checked: true},
                     {text: "Work closely with our legal advisors and investment bankers to optimize valuation", checked: true},
-                    {text: "Execute exit plan and achieve the best outcome for investors, shareholders and token holders", checked: false},
                 ].map((item, i) => (
                     <li key={i} className="flex items-start">
                         <div>
                             {item.checked ? (
                                 <FaCheckDouble className="text-xs text-purple-500 mr-2 mt-1.5"/>
                             ) : (
-                                <FaCircle className="text-xs mr-2 mt-1.5"/>
+                                <FaCircle className="text-[0.5rem] mr-3 mt-2"/>
                             )}
                         </div>
                         <div>
@@ -258,7 +252,8 @@ export default function RoadmapTimeline() {
                 <p>The XRPH token is a scarce asset, with only 100 million tokens ever created. The XRPH account has
                     been blackholed, ensuring that no additional tokens can ever be minted. This guarantees the token’s
                     limited supply and reinforces its value as a deflationary digital asset.
-                    Our focus remains on building trust and delivering long{FixText('-')}term value to our ecosystem, ensuring that
+                    <br/><br/>Our focus remains on building trust and delivering long{FixText('-')}term value to our ecosystem,
+                    ensuring that
                     XRP Healthcare continues to grow while honoring the commitments made to our community.</p>
             </div>
         </>
